6125d44f1b9e1d836202b98629eb7fac2995d12a
Yokesh T <yokeshyokesh19274@gmail.com>
2020-08-08 12:40:02 -0300

    first commit

--- map/settings.py
+++ map/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#2plx^rh%ld!2cnz#mwd#d9=jr4fxga+uz71n11_7fiqxa_mgo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vtg.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
